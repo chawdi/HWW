65741cbb6f064aaab4e08a655ae80a6a9561542e
Artemis <43904086+chawdi@users.noreply.github.com>
2023-08-25 16:51:07 +0300

    Add files via upload

---
+++ NewsPaper/manage.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        total_rating = self.posts.aggregate(models.Sum('rating'))['rating__sum'] or 0
+        total_rating += self.comments.aggregate(models.Sum('rating'))['rating__sum'] or 0
+        total_rating += self.posts.aggregate(models.Sum('comments__rating'))['comments__rating__sum'] or 0
+        self.rating = total_rating
+        self.save()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    POST_TYPES = (
+        ('article', 'Article'),
+        ('news', 'News'),
+    )
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=10, choices=POST_TYPES)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:124] + '...' if len(self.text) > 124 else self.text
+
+    def __str__(self):
+        return self.title
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.category.name}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
